aa7d526d572e3c80e1257f73cb06a130e681d8bf
Abdussobur Oluwabukola Kamardeen <kamardeenabdussobur@gmail.com>
2023-07-06 14:31:45 +0100

    new updates for my portfolio site

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 console.log("Hi, I am Abdussobur Kamardeen. Welcome to my site!")
+function menuToggle() {
+   var x = document.getElementById('myNavtoggle');
+   if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
