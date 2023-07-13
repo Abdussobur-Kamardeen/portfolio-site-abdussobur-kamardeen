7981fcec5fd767dd338b1b983407d60f5b5ef9cf
Abdussobur Oluwabukola Kamardeen <kamardeenabdussobur@gmail.com>
2023-07-13 20:34:01 +0100

    new changes made

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 console.log("Hi, I am Abdussobur Kamardeen. Welcome to my site!")
 function menuToggle() {
    var x = document.getElementById('myNavtoggle');
-   if (x.className === 'navtoggle') {
+   if (x.className === 'navtoggle' && $(window).width() < 640) {
     x.className += ' responsive';
   } else {
     x.className = 'navtoggle';
